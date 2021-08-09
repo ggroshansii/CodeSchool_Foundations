34f0c94e5112cf0799f311304d612769088d94e5
Mady Chapman <MC3D@users.noreply.github.com>
2021-08-09 09:54:55 -0400

    Update exercises.js

--- assignments/js/exercises.js
+++ assignments/js/exercises.js
@@ -6,7 +6,7 @@ function addNumbers(numberA, numberB) {
   console.log(numberA + numberB);
 }
 
-var twoPlusTwo = addNumbers(2, 2)
+const twoPlusTwo = addNumbers(2, 2)
 
 console.log(twoPlusTwo) // 4
 
